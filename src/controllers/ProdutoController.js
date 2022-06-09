e97de53921f43f51658eb4319c6d5ee706ba3810
Valdir Ferreira <valdirferreira2006@gmail.com>
2022-06-08 22:05:11 -0300

    Inclusão dos Cors

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -15,6 +15,11 @@ module.exports =
     },
 
     async Create(req, res) {
+              //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+    res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+        
         try {
             const produtos = await ModelProduto.create(
                 {
