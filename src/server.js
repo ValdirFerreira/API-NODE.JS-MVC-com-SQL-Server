fc30d46f6a3193061abc6cbbaa72d9c77a2fdbed
Valdir Ferreira <valdirferreira2006@gmail.com>
2022-06-08 22:05:47 -0300

    Inclusão do cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const api = express();
 const routes = require('./router');
+const cors = require('cors');
 
+api.use(cors());
 api.use(express.json());
 api.use(routes);
+
+
 
 api.listen(4200);
